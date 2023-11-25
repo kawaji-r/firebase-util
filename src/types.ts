e4ce433a977ab7c5c9c75182987224f03b601cee
kawaji/ubuntu <kawaji.r.333@gmail.com>
2023-11-25 21:50:39 +0900

    firebaseConfigTypeをoptionalに変更

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 import { WhereFilterOp } from 'firebase/firestore'
 
 export type firebaseConfigType = {
-  apiKey: string
-  authDomain: string
-  projectId: string
-  storageBucket: string
-  messagingSenderId: string
-  appId: string
+  apiKey?: string
+  authDomain?: string
+  projectId?: string
+  storageBucket?: string
+  messagingSenderId?: string
+  appId?: string
 }
 export type firestoreWhere = { key: string; operator: WhereFilterOp; value: any }
 export type firestoreField = { [key: string]: any }
