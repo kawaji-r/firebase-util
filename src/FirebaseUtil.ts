4377117ccac8e6fb23496d91672f391d7be31b05
kawaji/ubuntu <kawaji.r.333@gmail.com>
2023-11-25 12:28:16 +0900

    コメントを追加

--- src/FirebaseUtil.ts
+++ src/FirebaseUtil.ts
@@ -16,28 +16,32 @@ import {
 } from 'firebase/firestore'
 import { firebaseConfigType, FirestoreStructure } from './types'
 
+/**
+ * Firebaseのユーティリティクラス
+ * Firebaseの操作を抽象化し、簡単に利用できるようにするためのクラスです。
+ */
 class FirebaseUtil {
-  private readonly app: FirebaseApp
-  private readonly firestore: Firestore
+  private readonly app: FirebaseApp  // Firebaseアプリのインスタンス
+  private readonly firestore: Firestore  // Firestoreのインスタンス
 
+  /**
+   * FirebaseUtilのコンストラクタ
+   * @param {firebaseConfigType} config - Firebaseの設定
+   * Firebaseの設定を元に、FirebaseアプリとFirestoreを初期化します。
+   */
   constructor(config: firebaseConfigType) {
-    this.app = this.init(config)
-    this.firestore = getFirestore(this.app)
+    this.app = this.init(config)  // Firebaseアプリの初期化
+    this.firestore = getFirestore(this.app)  // Firestoreの初期化
   }
 
   /**
-   * | collection | document | field | next | operation    |
-   * | o          | o        | o    | o    | 登録して次へ |
-   * | o          | o        | o    | x    | 登録して終了 |
-   * | o          | o        | x    | o    | 次へ         |
-   * | o          | null     | o    | o    | 登録して次へ |
-   * | o          | null     | o    | x    | 登録して終了 |
-   * | o          | null     | x    | o    | 次へ         |
-   * 上記以外はエラー
-   * @param firestoreStructure
-   * @param ref
-   * @param mergeFlg
-   * @private
+   * Firestoreのフィールドを更新します
+   * @param {FirestoreStructure} firestoreStructure - Firestoreの構造
+   * @param {boolean} mergeFlg - マージフラグ
+   * @param {any} ref - Firestoreの参照
+   * Firestoreのドキュメントのフィールドを更新します。ドキュメントが存在しない場合は新規作成します。
+   * マージフラグがtrueの場合、既存のフィールドは保持され、指定したフィールドのみが更新されます。
+   * マージフラグがfalseの場合、ドキュメント全体が新たに書き換えられます。
    */
   public async updateField(
     firestoreStructure: FirestoreStructure,
@@ -45,41 +49,49 @@ class FirebaseUtil {
     ref: any = this.firestore
   ) {
     const obj = {
-      col: firestoreStructure.collection,
-      doc: firestoreStructure.document,
-      fld: firestoreStructure.field,
-      nxt: firestoreStructure.next
+      col: firestoreStructure.collection,  // コレクション名
+      doc: firestoreStructure.document,  // ドキュメントID
+      fld: firestoreStructure.field,  // 更新するフィールドのデータ
+      nxt: firestoreStructure.next  // 次に更新するFirestoreの構造
     }
 
     if (!obj.col) throw new Error(`collectionキーは必須です / ${JSON.stringify(obj)}`)
     if (!obj.fld && !obj.nxt) throw new Error(`fieldキーとnextキーのどちらかは必須です / ${JSON.stringify(obj)}`)
 
     if (obj.doc && obj.fld && obj.nxt) {
-      ref = doc(ref, obj.col, obj.doc)
-      await setDoc(ref, obj.fld, { merge: mergeFlg })
-      await this.updateField(obj.nxt, mergeFlg, ref)
+      ref = doc(ref, obj.col, obj.doc)  // ドキュメントの参照を取得
+      await setDoc(ref, obj.fld, { merge: mergeFlg })  // フィールドを更新
+      await this.updateField(obj.nxt, mergeFlg, ref)  // 次の更新を実行
     } else if (obj.doc && obj.fld && !obj.nxt) {
-      ref = doc(ref, obj.col, obj.doc)
-      await setDoc(ref, obj.fld, { merge: mergeFlg })
+      ref = doc(ref, obj.col, obj.doc)  // ドキュメントの参照を取得
+      await setDoc(ref, obj.fld, { merge: mergeFlg })  // フィールドを更新
     } else if (obj.doc && !obj.fld && obj.nxt) {
-      ref = doc(ref, obj.col, obj.doc)
-      await this.updateField(obj.nxt, mergeFlg, ref)
+      ref = doc(ref, obj.col, obj.doc)  // ドキュメントの参照を取得
+      await this.updateField(obj.nxt, mergeFlg, ref)  // 次の更新を実行
     } else if (!obj.doc && obj.fld && obj.nxt) {
-      ref = collection(ref, obj.col)
-      ref = await addDoc(ref, obj.fld)
-      await this.updateField(obj.nxt, mergeFlg, ref)
+      ref = collection(ref, obj.col)  // コレクションの参照を取得
+      ref = await addDoc(ref, obj.fld)  // 新規ドキュメントを作成
+      await this.updateField(obj.nxt, mergeFlg, ref)  // 次の更新を実行
     } else if (!obj.doc && obj.fld && !obj.nxt) {
-      ref = collection(ref, obj.col)
-      await addDoc(ref, obj.fld)
+      ref = collection(ref, obj.col)  // コレクションの参照を取得
+      await addDoc(ref, obj.fld)  // 新規ドキュメントを作成
     } else if (!obj.doc && !obj.fld && obj.nxt) {
-      ref = collection(ref, obj.col)
-      ref = await addDoc(ref, {})
-      await this.updateField(obj.nxt, mergeFlg, ref)
+      ref = collection(ref, obj.col)  // コレクションの参照を取得
+      ref = await addDoc(ref, {})  // 空の新規ドキュメントを作成
+      await this.updateField(obj.nxt, mergeFlg, ref)  // 次の更新を実行
     } else {
       throw new Error(`インプットデータが不正です / ${JSON.stringify(obj)}`)
     }
   }
 
+  /**
+   * Firestoreのドキュメントを読み込みます
+   * @param {FirestoreStructure} firestoreStructure - Firestoreの構造
+   * @param {any} ref - Firestoreの参照
+   * @returns {Promise<{id: string, data: unknown}[] | null>} ドキュメントのデータ
+   * Firestoreのドキュメントを読み込み、そのデータを返します。
+   * ドキュメントが存在しない場合はnullを返します。
+   */
   public async readDocument(
     firestoreStructure: FirestoreStructure,
     ref: any = this.firestore
@@ -91,10 +103,10 @@ class FirebaseUtil {
     | null
   > {
     const obj = {
-      col: firestoreStructure.collection,
-      doc: firestoreStructure.document,
-      and: firestoreStructure.whereAnd,
-      nxt: firestoreStructure.next
+      col: firestoreStructure.collection,  // コレクション名
+      doc: firestoreStructure.document,  // ドキュメントID
+      and: firestoreStructure.whereAnd,  // 検索条件
+      nxt: firestoreStructure.next  // 次に読み込むFirestoreの構造
     }
 
     if (!obj.col) throw new Error(`collectionキーは必須です / ${JSON.stringify(obj)}`)
@@ -104,12 +116,12 @@ class FirebaseUtil {
     let result: { id: string; data: unknown }[] | null = []
 
     if (obj.doc) {
-      ref = doc(ref, obj.col, obj.doc)
-      const docSnap = await getDoc(ref)
+      ref = doc(ref, obj.col, obj.doc)  // ドキュメントの参照を取得
+      const docSnap = await getDoc(ref)  // ドキュメントを読み込む
       if (docSnap.exists()) {
         result.push({
           id: obj.doc,
-          data: docSnap.data()
+          data: docSnap.data()  // ドキュメントのデータを取得
         })
       } else {
         result = null
@@ -117,52 +129,69 @@ class FirebaseUtil {
     }
 
     if (obj.and) {
-      let q: Query<unknown, DocumentData> = collection(ref, obj.col)
+      let q: Query<unknown, DocumentData> = collection(ref, obj.col)  // コレクションの参照を取得
       // 複数条件のAND検索を実現できている
       obj.and.forEach((el: any) => {
-        q = query(q, where(el.key, el.operator, el.value))
+        q = query(q, where(el.key, el.operator, el.value))  // 検索条件を追加
       })
-      const querySnapshot = await getDocs(q)
+      const querySnapshot = await getDocs(q)  // 検索を実行
       querySnapshot.forEach((document) => {
         // (whr && nxt)の場合は、whrの結果のドキュメントIDを使ってnxt処理に使用する。
         // それもあり、whrの結果は1つのみの想定。そうでない場合は任意の一つのドキュメントが採用される。
-        ref = doc(ref, obj.col, document.id)
+        ref = doc(ref, obj.col, document.id)  // ドキュメントの参照を取得
         result.push({
           id: document.id,
-          data: document.data()
+          data: document.data()  // ドキュメントのデータを取得
         })
       })
     }
 
     // nxt がある場合はrefのみ、ない場合は result のみが使われる
-    if (obj.nxt) return await this.readDocument(obj.nxt, ref)
+    if (obj.nxt) return await this.readDocument(obj.nxt, ref)  // 次の読み込みを実行
     return result
   }
 
+  /**
+   * Firestoreのドキュメントを削除します
+   * @param {FirestoreStructure} firestoreStructure - Firestoreの構造
+   * @param {any} ref - Firestoreの参照
+   * Firestoreのドキュメントを削除します。サブコレクションは削除されません。
+   */
   public async deleteDocument(firestoreStructure: FirestoreStructure, ref: any = this.firestore) {
     if (!firestoreStructure.collection) throw new Error('collectionキーは必須です')
     if (!firestoreStructure.document) throw new Error('documentキーは必須です')
 
-    ref = doc(ref, firestoreStructure.collection, firestoreStructure.document)
+    ref = doc(ref, firestoreStructure.collection, firestoreStructure.document)  // ドキュメントの参照を取得
     if (firestoreStructure.next) {
       // サブコレクションは削除されないので注意
-      await this.deleteDocument(firestoreStructure.next, ref)
+      await this.deleteDocument(firestoreStructure.next, ref)  // 次の削除を実行
     }
-    await deleteDoc(ref)
+    await deleteDoc(ref)  // ドキュメントを削除
   }
 
+  /**
+   * Firestoreのコレクションを削除します
+   * @throws {Error} Webクライアントからのコレクションの削除は推奨されていません
+   * Webクライアントからコレクションを削除することは推奨されていません。このメソッドはエラーをスローします。
+   */
   public deleteCollection(): void {
     throw new Error('Webクライアントからのコレクションの削除は推奨されていません')
   }
 
+  /**
+   * Firebaseアプリを初期化します
+   * @param {firebaseConfigType} config - Firebaseの設定
+   * @returns {FirebaseApp} Firebaseアプリ
+   * Firebaseの設定を元に、Firebaseアプリを初期化します。すでに初期化されている場合は、そのインスタンスを返します。
+   */
   private init(config: firebaseConfigType): FirebaseApp {
     let app: FirebaseApp
-    const apps: FirebaseApp[] = getApps()
+    const apps: FirebaseApp[] = getApps()  // すでに初期化されているFirebaseアプリのリストを取得
     if (!apps.length) {
       // Firebaseアプリが初期化されていなければ初期化する
       app = initializeApp(config)
     } else {
-      app = apps[0]
+      app = apps[0]  // すでに初期化されているFirebaseアプリのインスタンスを取得
     }
     return app
   }
